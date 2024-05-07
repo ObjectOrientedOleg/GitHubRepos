384037bb3f360e8c05b71149f46b5a83f2f3b9a4
ObjectOrientedOleg <osiboglov@gmail.com>
2024-05-06 21:09:02 -0400

    update kotlin and compose compiler versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
     }
     packaging {
         resources {
